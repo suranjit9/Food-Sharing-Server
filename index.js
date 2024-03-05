8032eca7160670e5a4033a9850e59487c2be4d34
Suranjit9 <suranjitm07@gmail.com>
2024-03-06 01:36:46 +0600

    Requast Food

--- index.js
+++ index.js
@@ -25,22 +25,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const UserData = client.db('Food-sharing').collection('Data');
-    const FoodData = client.db('Food-sharing').collection('AllFood');
+    const userData = client.db('Food-sharing').collection('Data');
+    const foodData = client.db('Food-sharing').collection('AllFood');
+    const requstfood = client.db('Food-sharing').collection('Requstfood');
     app.post('/user',async(req, res)=>{
-        const userdata = req.body;
-        const result = await UserData.insertOne(userdata);
+        const userData = req.body;
+        const result = await userData.insertOne(userData);
         res.send(result)
     })
     app.patch('/user', async(req, res)=>{
-        const userdata = req.body;
-        const UesrEmail = {email:userdata.email};
+        const userData = req.body;
+        const UesrEmail = {email:userData.email};
         const ubdateDoc ={
             $set:{
-              listTimeLogin:userdata.listTime
+              listTimeLogin:userData.listTime
             }
           }
-        const result = await UserData.updateOne(UesrEmail,ubdateDoc);
+        const result = await userData.updateOne(UesrEmail,ubdateDoc);
         res.send(result)
     })
     // User data get.........
@@ -54,22 +55,30 @@ async function run() {
        if (req.query?.email) {
         query = {email: req.query?.email}
        }
-       const result = await UserData.find(query).toArray();
+       const result = await userData.find(query).toArray();
       res.send(result)
     })
     // AddFood ..............
     app.post("/addFood", async(req, res)=>{
         const food = req.body;
-        const result = await FoodData.insertOne(food);
+        const result = await foodData.insertOne(food);
         res.send(result)
     })
-    // all Food Get....
     app.get('/addFood', async(req, res)=>{
+        
+        const find = foodData.find();
+        const result = await find.toArray();
+        // const result2 = result.slice(0, 8);
+        res.send(result);
+        })
+    // all Food Get.... Available Food page
+    app.get('/addFood/Available', async(req, res)=>{
         const filter = req.query;
         console.log(filter)
         const query = {
             // FoodStatus:{$in:"available"}
-            foodName:{$regex:filter.search, $options:'i'}
+            foodName:{$regex:filter.search, $options:'i'},
+            FoodStatus: { $ne: "Uavailable" }
         };
         const option = {
             sort: {
@@ -78,26 +87,39 @@ async function run() {
             },
           };
           console.log(option)
-        const result = await FoodData.find(query,option).toArray();
+        const result = await foodData.find(query,option, { "FoodStatus": { $ne: "Available" } }).toArray();
         res.send(result)
     })
-    // QTY High to Low---------
+    // QTY High to Low--------- Home page 
     app.get('/addFood/filter', async(req, res)=>{
-        const result = await FoodData.find().toArray();
+        const filter = req.query;
+        const query = {
+            // FoodStatus:{$in:"available"}
+            // Quantity:{$gt:5},
+            FoodStatus: { $ne: "Uavailable" },
+            foodName:{$regex:filter.search, $options:"i"}
+        };
+        const option = {
+            sort: {
+                Expired:1
+             
+            },
+          };
+        const result = await foodData.find(query, option).limit(12).toArray();
         res.send(result)
     })
     // FoodStatus & currant Time change.............
-    app.patch('/addFood/:id', async(req, res)=>{
+    app.patch('/addFood/time/:id', async(req, res)=>{
         const id = req.params.id;
         const Status = req.body;
         const filter = {_id: new ObjectId(id)};
         const updateDoc = {
             $set: {
-                FoodStatus: Status.FoodStatus,
-                Expired: Status.Expired
+                FoodStatus: Status.FoodStatus
+                // Expired: Status.Expired
             },
           };
-          const result = await FoodData.updateOne(filter, updateDoc)
+          const result = await foodData.updateOne(filter, updateDoc)
         res.send(result)
     })
     // ManageFoods................
@@ -108,8 +130,23 @@ async function run() {
         if (req.query?.email) {
          query = {email: req.query?.email}
         }
-        const result = await FoodData.find(query).toArray();
+        const result = await foodData.find(query).toArray();
        res.send(result) 
+    })
+    // SingalFood--------------
+    app.get('/addFood/SingalFood/:id', async(req,res)=>{
+        const id = req.params.id;
+        console.log(id)
+        const query = {_id: new ObjectId(id)};
+        const result = await foodData.findOne(query);
+        // console.log(result)
+        res.send(result)
+    })
+    // customer requstFood 
+    app.post(`/requstFood`, async (req, res)=>{
+        const query = req.body;
+        const result = await requstfood.insertOne(query);
+        res.send(result)
     })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
@@ -125,7 +162,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!Pitu')
 })
 
 app.listen(port, () => {
